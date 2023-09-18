d790547139ebbd906273ed7fce1fd904db4351e7
Griffin Stofko <gpstofko@cps.edu>
2023-09-18 11:32:37 -0500

    changes 9/18

--- a2.py
+++ a2.py
@@ -17,17 +17,41 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
     sind = 0  # current index we are looking at in source list
     pind = 0  # current index we are looking at in pattern list
     result: List[str] = []  # to store substitutions we will return if matched
-    if len(source) > len(pattern):
-        
-    if len(pattern) > len(source):
 
-    while sind < len(source) and pind < len(pattern):
-        if source[sind] == '%' or source[sind] == '_':
-            result.append(pattern[pind])
-        elif source[sind] != pattern[pind]:
+    # keep checking as long as we haven't hit the end of either pattern or source while
+    # pind is still a valid index OR sind is still a valid index (valid index means that
+    # the index is != to the length of the list)
+    while pind != len(pattern) or sind != len(source):
+        # your job is to fill out the body of this loop
+    
+        # you should delete the following line
+
+        # 1) if we reached the end of the pattern but not source
+        if pind == len(pattern) and sind != len(source):
             return None
-        sind += 1
-        pind += 1
+
+        # 2) if the current thing in the pattern is a %
+        # WARNING: this condition contains the bulk of the code for the assignment
+        # If you get stuck on this one, we encourage you to attempt the other conditions
+        #   and come back to this one afterwards
+        elif pattern[pind] == "%":
+            result.append(source[sind:])
+        # 3) if we reached the end of the source but not the pattern
+        elif sind == len(source) and pind != len(pattern):
+            return None
+        # 4) if the current thing in the pattern is an _
+        elif pattern[pind] == "_":
+            result.append(source[sind])
+        # 5) if the current thing in the pattern is the same as the current thing in the
+        # source
+        elif pattern[pind] == source[sind]:
+            pind += 1
+            sind += 1
+        # 6) else : this will happen if none of the other conditions are met it
+        # indicates the current thing it pattern doesn't match the current thing in
+        # source
+        else:
+            return None
     return result
 
 
